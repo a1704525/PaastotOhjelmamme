8e95e88c821901dcc9f94d0259184a902e371812
Miia Oksanen <miia.oksanen@myy.haaga-helia.fi>
2019-04-27 15:03:10 +0300

    Lisätty projekti

---
+++ PaastotOhjelma/src/main/java/Projektitehtava/PaastotOhjelma/PaastotOhjelmaApplication.java
@@ -0,0 +1,13 @@
+package Projektitehtava.PaastotOhjelma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaastotOhjelmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaastotOhjelmaApplication.class, args);
+	}
+
+}
